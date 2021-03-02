d46256c002d7f4c38dbb6a0e4a86e412bf4f5c8c
nataly garcia <naty_garcioli@hotmail.com>
2021-03-02 16:47:30 -0500

    added routes & trip_style Controller

---
+++ app/controllers/trips_styles_controller.rb
@@ -0,0 +1,14 @@
+class TripsStylesController < ApplicationController
+  def index
+    # http://localhost:3000/trips/1/trip_styles
+    @trip = Trip.find(params[:trip_id])
+    @trip_style = TripStyle.new(trip_style_params)
+    # @trip_style.trip = @trip
+  end
+
+  private
+
+  def trip_style_params
+    params.require(:trip_style).permit(:trip_id, :title)
+  end
+end
