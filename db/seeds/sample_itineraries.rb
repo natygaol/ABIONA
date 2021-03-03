e1c9cc52255a9b3a8709ab38b6befcd604b88979
Gonzalo Enei <gonzalo@fintual.com>
2021-03-03 14:44:42 -0300

    Begins work on seed

---
+++ db/seeds/sample_itineraries.rb
@@ -0,0 +1,14 @@
+sample_itineraries_seed = [
+  {
+    title: "Out of Africa: Romance and Safari",
+    description: "Lose yourself in the savannah in this classic, East African journey. Enjoy the Great Migration at its best, with a wonderful combination of lesiure and activities."
+    stops: {
+      1: { 
+        place: "Arusha",
+        accommodations:
+
+      }
+    }
+
+  }
+]
