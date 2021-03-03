2eb8980dc2b1bb04cec6b2175048e39a9c17a754
Santiago Almiron <sanalmiron@hotmail.com>
2021-03-03 17:35:40 -0300

    set date and group size

--- app/javascript/components/counter.js
+++ app/javascript/components/counter.js
@@ -4,19 +4,19 @@ const counter = (numberPlace, plusButton, restButton, hiddenInput) => {
     numberPlace.innerText = 0;
     hiddenInput.value = counter;
     plusButton.addEventListener("click", () => {
-        counter += 1;
-        numberPlace.innerText = counter;
-        hiddenInput.value = counter;
+      counter += 1;
+      numberPlace.innerText = counter;
+      hiddenInput.value = counter;
     });
     restButton.addEventListener("click", () => {
-        if (counter > 0) {
-            counter -= 1;
-            numberPlace.innerText = counter;
-            hiddenInput.value = counter;
-        } else {
-            numberPlace.innerText = 0;
-            hiddenInput.value = 0;
-        };
+      if (counter > 0) {
+        counter -= 1;
+        numberPlace.innerText = counter;
+        hiddenInput.value = counter;
+      } else {
+        numberPlace.innerText = 0;
+        hiddenInput.value = 0;
+      };
     });
   };
 } 
