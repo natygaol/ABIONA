e337e28279a5b65687cdb751b68127564b9d2e92
emi rodriguez <emirodriguez_025@hotmail.com>
2021-03-04 18:22:29 -0300

    suggested options only frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sample_itineraries/index'
   devise_for :users
   root to: 'pages#home'
   resource :trips, only: [:new, :create]
+  resources :sample_itineraries, only: [:index]
 end
