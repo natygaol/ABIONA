8e3a3eb5dccf89d81a8b7ad0add6ac22a3ec9aaa
nataly garcia <naty_garcioli@hotmail.com>
2021-03-09 09:04:36 -0500

    changes on trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,7 +7,7 @@ class Trip < ApplicationRecord
   validates :start_date, :end_date, :adults, presence: true
 
   def total_price
-    # 1 migration. anadir a sample itineraries las columnasd de children_price_per_night & adult_price_per_night
+    # 1 migration. anadir a sample itineraries las columnas de children_price_per_night & adult_price_per_night
     # 2 Actualizar las seeds // sample itineraries
     # A Al actualizar las noches de un trip stop debo actualizar el end date del trip
     #   1 En el trip stop controller en las acciones que actualizan sus nights deberia suceder esto
