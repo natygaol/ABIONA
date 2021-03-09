038da3e100748cb71a285e9e20b5fa65e48092a1
Gonzalo Enei <gonzalo@fintual.com>
2021-03-09 20:37:28 -0300

    Comments price method that breaks code

--- app/controllers/trip_stops_controller.rb
+++ app/controllers/trip_stops_controller.rb
@@ -5,7 +5,7 @@ class TripStopsController < ApplicationController
     @trip_stop = TripStop.find(params[:id])
     @trip_stop.increase_one_night!
     @trip_stop.save
-    @trip_stops.trip.update(end_date: start_date + trip.trip_stops.reduce(0) { |accumulator, trip_stop| accumulator + trip_stop.nights })
+    # @trip_stops.trip.update(end_date: start_date + trip.trip_stops.reduce(0) { |accumulator, trip_stop| accumulator + trip_stop.nights })
     redirect_to customize_path(@trip_stop.trip, anchor: "trip-stop-#{@trip_stop.id}")
   end
 
@@ -13,7 +13,7 @@ class TripStopsController < ApplicationController
     @trip_stop = TripStop.find(params[:id])
     @trip_stop.decrease_one_night!
     @trip_stop.save
-    @trip_stops.trip.update(end_date: start_date + trip.trip_stops.reduce(0) { |accumulator, trip_stop| accumulator + trip_stop.nights })
+    # @trip_stops.trip.update(end_date: start_date + trip.trip_stops.reduce(0) { |accumulator, trip_stop| accumulator + trip_stop.nights })
     redirect_to customize_path(@trip_stop.trip, anchor: "trip-stop-#{@trip_stop.id}")
   end
 
