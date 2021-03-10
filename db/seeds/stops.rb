bf03c0243ef782de3bff597be0692bc9982aeab2
Gonzalo Enei <gonzalo@fintual.com>
2021-03-10 14:50:44 -0300

    Adds style to Activities and responsive behavior

--- db/seeds/stops.rb
+++ db/seeds/stops.rb
@@ -1,30 +1,30 @@
 STOPS = {
           dream_couple_active_dry: [
-                    { nights: 1, place: "Arusha", accommodation: :rivertrees },
-                    { nights: 2, place: "Manyara", accommodation: :manyara_green_camp },
-                    { nights: 1, place: "Ngorogoro", accommodation: :mysigio_camp },
-                    { nights: 2, place: "Serengeti", accommodation: :serengeti_green_camp },
-                    { nights: 2, place: "Serengeti Remote", accommodation: :wayo_walking_camp }
+                    { nights: 1, place: "Arusha Town", accommodation: :rivertrees },
+                    { nights: 2, place: "Manyara National Park", accommodation: :manyara_green_camp },
+                    { nights: 1, place: "Ngorongoro Crater", accommodation: :mysigio_camp },
+                    { nights: 2, place: "Serengeti National Park", accommodation: :serengeti_green_camp },
+                    { nights: 2, place: "Serengeti Walking Area", accommodation: :wayo_walking_camp }
           ],
          getaway_couple_romantic_dry: [
-                    { nights: 2, place: "Arusha", accommodation: :rivertrees },
-                    { nights: 1, place: "Manyara", accommodation: :manyara_green_camp },
-                    { nights: 2, place: "Ngorogoro", accommodation: :mysigio_camp },
-                    { nights: 3, place: "Serengeti", accommodation: :serengeti_green_camp },
+                    { nights: 2, place: "Arusha Town", accommodation: :rivertrees },
+                    { nights: 1, place: "Manyara National Park", accommodation: :manyara_green_camp },
+                    { nights: 2, place: "Ngorongoro Crater", accommodation: :mysigio_camp },
+                    { nights: 3, place: "Serengeti National Park", accommodation: :serengeti_green_camp },
                     { nights: 5, place: "Zanzibar", accommodation: :sharazad }
           ],
           family_trip_dry: [
-                    { nights: 2, place: "Arusha", accommodation: :rivertrees },
-                    { nights: 2, place: "Manyara", accommodation: :manyara_green_camp },
-                    { nights: 2, place: "Ngorogoro", accommodation: :mysigio_camp },
-                    { nights: 3, place: "Serengeti", accommodation: :serengeti_green_camp },
+                    { nights: 2, place: "Arusha Town", accommodation: :rivertrees },
+                    { nights: 2, place: "Manyara National Park", accommodation: :manyara_green_camp },
+                    { nights: 2, place: "Ngorongoro Crater", accommodation: :mysigio_camp },
+                    { nights: 3, place: "Serengeti National Park", accommodation: :serengeti_green_camp },
                     { nights: 4, place: "Zanzibar", accommodation: :sharazad }
           ],
           couples_walking_safari: [
-                    { nights: 2, place: "Arusha", accommodation: :rivertrees },
-                    { nights: 2, place: "Manyara", accommodation: :manyara_green_camp },
-                    { nights: 2, place: "Ngorogoro", accommodation: :mysigio_camp },
-                    { nights: 2, place: "Serengeti", accommodation: :serengeti_green_camp },
+                    { nights: 2, place: "Arusha Town", accommodation: :rivertrees },
+                    { nights: 2, place: "Manyara National Park", accommodation: :manyara_green_camp },
+                    { nights: 2, place: "Ngorongoro Crater", accommodation: :mysigio_camp },
+                    { nights: 2, place: "Serengeti National Park", accommodation: :serengeti_green_camp },
                     { nights: 3, place: "Zanzibar", accommodation: :sharazad }
           ],
         }
