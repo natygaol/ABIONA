1bca9b41da0e95fb71858e3c1493be0856e31310
nataly garcia <naty_garcioli@hotmail.com>
2021-03-10 11:55:08 -0500

    fixed navbar innerHeight behaviour

--- app/javascript/components/navbar_ajax.js
+++ app/javascript/components/navbar_ajax.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-ajax');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight - 190) {
+      if (window.scrollY >= window.innerHeight - 200) {
         navbar.classList.add('navbar-ajax-white');
       } else {
         navbar.classList.remove('navbar-ajax-white');
