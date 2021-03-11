7730fab5c73653511a9a5f4c8aca91543b83cafc
nataly garcia <naty_garcioli@hotmail.com>
2021-03-11 12:48:46 -0500

    fixed cards, create container/grid for card, fixed-display-pic

--- app/controllers/sample_itineraries_controller.rb
+++ app/controllers/sample_itineraries_controller.rb
@@ -3,6 +3,5 @@ class SampleItinerariesController < ApplicationController
     @trip = Trip.find(params[:trip_id])
     @sample_itineraries = SampleItinerary.joins(:sample_itinerary_travel_styles)
                                          .where(sample_itinerary_travel_styles: {travel_style_id: @trip.travel_style_ids}).distinct
-    
   end
 end
